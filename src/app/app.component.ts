cb1c02b84057f328f830cca4c0773654fb42bbad
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-07-12 16:11:02 +0200

    Initial app polymer layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { PolymerElement } from '@vaadin/angular2-polymer'
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [
+    PolymerElement('app-header-layout'),
+    PolymerElement('app-header'),
+    PolymerElement('app-toolbar')
+  ]
 })
 export class AppComponent {
   title = 'app works!';
